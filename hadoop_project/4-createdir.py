d74cfe3d113fb223b987599c3bcd12091f982b93
vigobin <vgobin@outlook.com>
2024-03-11 12:09:59 -0500

    loop through dir

---
+++ hadoop_project/4-createdir.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.7
+"""Task 3. HDFS with Python (Snakebite): createdir"""
+import snakebite.client as sb
+
+
+def createdir(l):
+    """Creates the directories listed on l within HDFS."""
+    client = sb.Client("localhost", 9000)
+    for dir_name in client.mkdir(l, create_parent=True):
+        print(dir_name)
+
+
+if __name__ == "__main__":
+    createdir(["/holbies/input"])
---
+++ hadoop_project/4-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2.7
+
+
+createdir = __import__('4-createdir').createdir
+
+
+l = ["/Betty", "/Betty/Holberton"]
+createdir(l)
